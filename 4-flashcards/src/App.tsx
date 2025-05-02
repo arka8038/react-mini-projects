0b8a6a947ce7ab3d1e678eea8118f2e81481be92
arka8038 <logicarka@gmail.com>
2025-05-02 15:55:31 +0530

    refactor: remove unused state management from App component

--- 4-flashcards/src/App.tsx
+++ 4-flashcards/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import FlashCards from './FlashCards'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='app'>
       <FlashCards />
